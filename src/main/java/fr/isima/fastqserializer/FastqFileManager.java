1c770be7d9c92d69c9a1af4c6cae2d4ee5b557ca
Elysée <jonas.elysee@gmail.com>
2016-11-25 10:33:04 +0100

    Changement du serializeur par défaut Ajout des algorithmes suivant: FIltrage Simpling Calculs de statistiques (sauf par position de nucleotides)
    Ce qui manque:
    Trimming

--- src/main/java/fr/isima/fastqserializer/FastqFileManager.java
+++ src/main/java/fr/isima/fastqserializer/FastqFileManager.java
@@ -7,6 +7,8 @@ import java.io.File;
 import java.io.FilenameFilter;
 import java.io.IOException;
 import java.io.Serializable;
+import java.nio.file.FileSystems;
+import java.nio.file.Files;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Iterator;
@@ -116,7 +118,7 @@ public class FastqFileManager implements Serializable{
 		fastqReader.close();
 	}
 
-	public void convertFastqToFqrdd(JavaSparkContext sc, String filePath) throws IOException {
+	public void convertFastqToFqrdd(JavaSparkContext sc, String filePath, String resultFolder) throws IOException {
 		
 		List<FastqRecord> fastqArray = getFqArray(filePath);
 		
@@ -128,14 +130,18 @@ public class FastqFileManager implements Serializable{
 
 		System.out.println("Exportation");
 		// si le dossier existe déjà, il lance une erreur
-		fastqRDD.saveAsObjectFile("./results/SP1.fqrdd");
+		fastqRDD.saveAsObjectFile(resultFolder );
 		//fastqRDD.saveAsObjectFile("./results/temp/SP1.fqrdd");
 	}
 	
 	public void getFqRDDSatistics(JavaSparkContext sc, String folderPath) throws IOException{
 		System.out.println("Début");
 		//List<FastqRecord> fastqArray = getFqArray(filePath);
-		JavaRDD<FastqRecord> fqrdd = readFqRDDFolder(sc,folderPath);
+		//JavaRDD<FastqRecord> fqrdd = readFqRDDFolder(sc,folderPath);
+		
+		// TOTEST
+		
+		JavaRDD<FastqRecord> fqrdd =filterFqRdd(sc, folderPath, 0, Integer.MAX_VALUE, 0, Integer.MAX_VALUE,true);
 		/* We want to know:
 
 		    number of:
@@ -160,14 +166,20 @@ public class FastqFileManager implements Serializable{
 		nbEntries = fqrdd.count();
 		System.out.println("Number of entries: "+nbEntries);
 		
+		/*
+		 * Récupération des séquences lues
+		 */
 		JavaRDD<String> sequences = fqrdd.flatMap(new FlatMapFunction<FastqRecord, String>(){
 			public Iterator<String> call(FastqRecord r) {
 				List<String> list = Arrays.asList( r.getReadString().split(" "));
 				Iterable<String> iter = list;
-				return iter.iterator();   // Recupération des séquences
+				return iter.iterator(); 
 			}
-		});
+ 		});
 		
+		/*
+		 * Isolation de chaque Nucléotides dans toutes les séquences
+		 */
 		 JavaRDD<String> nucleotides = sequences.flatMap(new FlatMapFunction<String, String>(){
 		 
 			public Iterator<String> call(String s){
@@ -177,22 +189,50 @@ public class FastqFileManager implements Serializable{
 			}
 		});
 		
+		 /* 
+		  * Compte de chaque nucléotides
+		  */
+		 //Map
 		JavaPairRDD<String,Integer> pairs = nucleotides.mapToPair(new PairFunction<String, String, Integer>(){
 			public Tuple2<String,Integer> call(String s){
 				return new Tuple2<String,Integer>(s,1);
 			}
 		});
 		
+		//reduce
 		JavaPairRDD<String, Integer> counts = pairs.reduceByKey(new Function2<Integer, Integer, Integer>() {
 			  public Integer call(Integer a, Integer b) { return a + b; }
 		});
+		/* ********************************* */
 		
+		
+		/*
+		 * Récupération de la qualité moyenne de chaque séquences
+		 */
 		JavaPairRDD<String,Integer> meanSequencesQualities = fqrdd.mapToPair(new PairFunction<FastqRecord, String,Integer>(){
 			public Tuple2<String, Integer> call(FastqRecord r) {
 				return new Tuple2<String,Integer> (r.getReadString(), getSequenceQuality(r)/r.length()) ;  // Recupération des séquences
 			}
 		});
 		/* *************************** */
+		
+		/*
+		 * Récupération des distributions
+		 */
+		// MAP
+		JavaPairRDD<Integer,Integer> distributions  = sequences.mapToPair(new PairFunction<String, Integer , Integer>(){
+			public Tuple2<Integer,Integer> call(String s){
+				return new Tuple2<Integer,Integer>(s.length(),1);
+			}
+		});
+		
+		//REDUCE
+		JavaPairRDD<Integer, Integer> distLenght = distributions.reduceByKey(new Function2<Integer, Integer, Integer>() {
+			  public Integer call(Integer a, Integer b) { return a + b; }
+		});
+		
+		/* ****************************************** */
+		
 		//<Seq,Quality>
 		JavaPairRDD<String,String> SeqQualities = fqrdd.mapToPair(new PairFunction<FastqRecord, String,String>(){
 			public Tuple2<String, String> call(FastqRecord r) {
@@ -214,7 +254,7 @@ public class FastqFileManager implements Serializable{
 		System.out.println("Sequences and Mean Quality");
 		meanSequencesQualities.foreach(new VoidFunction<Tuple2<String, Integer>>(){
 			public void call(Tuple2<String, Integer> t){
-				System.out.println(t._1 + " -> "+t._2);
+				//System.out.println(t._1 + " -> "+t._2);
 			}
 		} );
 		
@@ -230,24 +270,101 @@ public class FastqFileManager implements Serializable{
 		System.out.println("Sequences and length");
 		sequences.foreach(new VoidFunction<String>(){
 			public void call(String s){
-				System.out.println(s+ " : " + s.length());
+			//	System.out.println("X"+ " -> " + s.length());
 			}
 		});
 		
+		System.out.println("------------------------------");
+		System.out.println("Distributions found");
+		distLenght.foreach(new VoidFunction<Tuple2<Integer, Integer>>(){
+			public void call(Tuple2<Integer, Integer> t){
+				System.out.println(t._1 + " -> "+t._2);
+			}
+		} );
 		
 	}
 	
-	public void filterFqRdd(JavaSparkContext sc, String filepath) throws IOException{
-		List<FastqRecord> fastqArray = getFqArray(filepath);
-		JavaRDD<FastqRecord> fqrdd = sc.parallelize(fastqArray);
+	public JavaRDD<FastqRecord> filterFqRdd(JavaSparkContext sc, String folderPath,
+			final int minLength,final int maxLength, final int minQual, final int maxQual, boolean atgc) throws IOException{
+		JavaRDD<FastqRecord> fqrdd = readFqRDDFolder(sc,folderPath);
 		
-		// EXEMPLE: Filtre par rapport à la longueur de la séquence par exemple
 		JavaRDD<FastqRecord> fltrRes = fqrdd.filter(new Function<FastqRecord,Boolean> (){
 			public Boolean call (FastqRecord fq ){
-				return fq.getReadString().length() > 10;  
+				int quality = getSequenceQuality(fq);
+				int len = fq.getReadString().length();
+				return (len >= minLength)
+						&& (len <= maxLength)
+						&& (quality >= minQual)
+						&& (quality <= maxQual);  
 			}
 		});
 		
+		if(atgc){
+			fltrRes = fltrRes.filter(new Function<FastqRecord, Boolean>(){
+				public Boolean call (FastqRecord fq){
+					//return !fq.getReadString().contains("N"); // retourne que si il y a atgc	
+					String seqLeft = fq.getReadString().replace("A", "")
+							.replace("T", "")
+							.replace("G", "")
+							.replace("C", "");
+					boolean res = !(seqLeft
+							.length() > 0); //TODO Test
+					
+					//System.out.println("SEQ LEFT!! -- ." + seqLeft +'.');
+					return res;
+				}
+			});
+		}
+		return fltrRes;
+		//return le truc filtré
+	}
+	
+	//TODO Test
+	public void filterFqRddwID(JavaSparkContext sc, String folderPath, String idFilePath,
+			final int minLength,final int maxLength, final int minQual, final int maxQual, boolean atgc) throws IOException{
+		//JavaRDD<FastqRecord> fqrdd = readFqRDDFolder(sc,folderPath);
+			
+		File headerFile = new File(idFilePath);
+		final List<String> headers = Files.readAllLines(FileSystems.getDefault().getPath(idFilePath));
+		
+		JavaRDD<FastqRecord>  fqrdd = filterFqRdd(sc, folderPath, minLength, maxLength, minQual, maxQual, atgc);
+		JavaRDD<FastqRecord>  fltrRes = fqrdd.filter(new Function<FastqRecord,Boolean> (){
+			public Boolean call (FastqRecord fq ){
+				return headers.contains(fq.getReadHeader());
+				
+			}
+		});
+		
+		
+		// return fltrRes;
+		
+	}
+	
+	public void sampleFqRdd(JavaSparkContext sc, String folderPath, int nb, int seed) throws IOException
+	{
+		JavaRDD<FastqRecord> fqrdd = readFqRDDFolder(sc,folderPath);
+		
+		double percent  = nb / fqrdd.count();
+		JavaRDD<FastqRecord> fqresult = fqrdd.sample(false, percent, seed);
+		
+		// return fqresult;
+	
+	}
+	
+	public void trimFqRdd (JavaSparkContext sc, String folderPath, int minQuality, int windowSize) throws IOException{
+		JavaRDD<FastqRecord> fqrdd = readFqRDDFolder(sc,folderPath);
+		// TODO  A savoir si les séquences qui n'ont pas la minimum window size sont exclu
+		//  A savoir si lorsqu'on ne retrouve pas la min seq  ce qu'on fait
+		
+		/*
+		 * Algorithme de principe
+		 * Tant que la séquence total traité à un minimum de window size
+		 * 	calculer la qualité de la séquence dans la windowsize
+		 * 	si ce n'est pas bon on enlève l'extrémité et on continu
+		 * SI C BON? ON CONTINU?
+		 * SI C PAS BON AU MILIEU??
+		 * 
+		 */
 	}
 	
 	
--- src/main/java/fr/isima/fastqserializer/HelloSpark.java
+++ src/main/java/fr/isima/fastqserializer/HelloSpark.java
@@ -1,6 +1,9 @@
 package fr.isima.fastqserializer;
 
+import htsjdk.samtools.fastq.FastqRecord;
+
 import java.io.IOException;
+import java.util.Arrays;
 
 import org.apache.spark.api.java.*;
 import org.apache.spark.*;
@@ -10,7 +13,11 @@ public class HelloSpark {
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
 		
-		SparkConf conf = new SparkConf().setAppName("Simple Application").setMaster("local[2]");
+		SparkConf conf = new SparkConf().setAppName("Simple Application").setMaster("local[2]")
+				.set("spark.driver.maxResultSize", "0")
+				.set("spark.rdd.compress", "true")
+				.set("spark.serializer", "org.apache.spark.serializer.KryoSerializer")
+				.registerKryoClasses((Class<?>[]) Arrays.asList(FastqRecord.class).toArray());  
 		JavaSparkContext sc = new JavaSparkContext(conf);
 		
 		sc.setLogLevel("OFF"); //ALL, DEBUG, ERROR, FATAL, INFO, OFF, TRACE, WARN
@@ -20,13 +27,14 @@ public class HelloSpark {
 		
 		
 		//System.out.println("Goodbye.");
-		String filePath = "./data/SP1.fq";
-		String folderPath = "./results/SP1.fqrdd";
+		String filePath = "./data/SP2.fq";
+		String folderPath = "./results/SP2.fqrdd";
+		String resultPath = "./results/SP2.fqrdd";
 		FastqFileManager fqman = new FastqFileManager();
 		
 		try {
 			//fqman.readFastqFile(filePath);
-			//fqman.convertFastqToFqrdd(sc,filePath);
+			//fqman.convertFastqToFqrdd(sc,filePath,resultPath);
 			fqman.getFqRDDSatistics(sc,folderPath);
 			//fqman.readFqRDD(sc,"./results/SP1.fqrdd/part-00000");
 			
